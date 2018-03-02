f0bb949f2ac741c8c6fbd11d018529d6ab3f3966
dongjingfeng <dongjf@tydic.com>
2018-03-02 16:28:43 +0800

    ajax获取数据

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import AJAX from './ajax/ajax'
 export default {
   name: 'app',
   data :function(){
-    let data = {
+    /*let data = {
       config:{
         url:"getMianData",
         method:"get"
@@ -27,42 +27,60 @@ export default {
         console.log("errorF执行成功");
       }
     };
-    AJAX.ajax(data);
-    /*axios.get('getMianData', {
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (response) {
-      console.log(response);
-    });*/
+    AJAX.ajax(data);*/
+    /*
+    * options:{
+     mainOptions:{
+     navName:"vue测试系统",
+     mainOperation:[
+     {
+     title:"邮箱",
+     icon:"./assets/logo.png",
+     page:"Page1"
+     },
+     {
+     title:"任务",
+     icon:"./assets/logo.png"
+     },
+     {
+     title:"信息",
+     icon:"./assets/logo.png"
+     },
+     {
+     title:"用户",
+     icon:"./assets/logo.png"
+     }
+
+     ]
+     }
+     }*/
     return {
       options:{
-              mainOptions:{
-                navName:"vue测试系统",
-                mainOperation:[
-                  {
-                    title:"邮箱",
-                    icon:"./assets/logo.png",
-                    page:"Page1"
-                  },
-                  {
-                    title:"任务",
-                    icon:"./assets/logo.png"
-                  },
-                  {
-                    title:"信息",
-                     icon:"./assets/logo.png"
-                  },
-                  {
-                    title:"用户",
-                     icon:"./assets/logo.png"
-                  }
-
-                ]
-              }
-            }
+        mainOptions: {}
+      }
     }
+  },
+  created:function(){
+    var _this = this;
+    let data = {
+      config:{
+        url:"getMianData",
+        method:"get"
+      },
+      successF:function(returnData){
+        console.log(_this.options);
+        console.log(returnData)
+        _this.options.mainOptions = returnData.data;
+
+      },
+      failF:function(returnData){
+        console.log("fialF执行成功");
+      },
+      errorF:function(){
+        console.log("errorF执行成功");
+      }
+    };
+    AJAX.ajax(data);
   },
   components:{mainNav}
 }
