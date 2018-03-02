9f8fb58a07fe669e6b7689790e97326c82707408
dongjingfeng <dongjf@tydic.com>
2018-03-02 16:42:18 +0800

    ajax获取数据

--- src/App.vue
+++ src/App.vue
@@ -6,95 +6,71 @@
 </template>
 
 <script>
-import mainNav from './components/mainNav'
-import axios from 'axios'
-import AJAX from './ajax/ajax'
-export default {
-  name: 'app',
-  data :function(){
-    let data = {
-      config:{
-        url:"getMianData",
-        method:"get"
-      },
-      successF:function(returnData){
-        console.log("successF执行成功");
-      },
-      failF:function(returnData){
-        console.log("fialF执行成功");
-      },
-      errorF:function(){
-        console.log("errorF执行成功");
+  import mainNav from './components/mainNav'
+  import axios from 'axios'
+  import AJAX from './ajax/ajax'
+  export default {
+    name: 'app',
+    data :function(){
+      return {
+        options:{
+          mainOptions: {}
+        }
       }
-    };
-    AJAX.ajax(data);
-    /*axios.get('getMianData', {
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (response) {
-      console.log(response);
-    });*/
-    return {
-      options:{
-              mainOptions:{
-                navName:"vue测试系统",
-                mainOperation:[
-                  {
-                    title:"邮箱",
-                    icon:"./assets/logo.png",
-                    page:"Page1"
-                  },
-                  {
-                    title:"任务",
-                    icon:"./assets/logo.png"
-                  },
-                  {
-                    title:"信息",
-                     icon:"./assets/logo.png"
-                  },
-                  {
-                    title:"用户",
-                     icon:"./assets/logo.png"
-                  }
+    },
+    created:function(){
+      var _this = this;
+      let data = {
+        config:{
+          url:"getMianData",
+          method:"get"
+        },
+        successF:function(returnData){
+          console.log(_this.options);
+          console.log(returnData)
+          _this.options.mainOptions = returnData.data;
 
-                ]
-              }
-            }
-    }
-  },
-  components:{mainNav}
-}
+        },
+        failF:function(returnData){
+          console.log("fialF执行成功");
+        },
+        errorF:function(){
+          console.log("errorF执行成功");
+        }
+      };
+      AJAX.ajax(data);
+    },
+    components:{mainNav}
+  }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
-body,p,ul{
-  margin:0;
-  padding:0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+  body,p,ul{
+    margin:0;
+    padding:0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
